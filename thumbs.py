2dbf5d4a21b3194c3a547380242261b73b37125c
Swarnadeep Chatterjee <swarnadeep.chatterjee@gmail.com>
2018-09-05 11:02:02 +0530

    Update thumbs.py

--- thumbs.py
+++ thumbs.py
@@ -7,12 +7,12 @@ from requests.auth import HTTPBasicAuth
 def getfile(filename):
     try:
         with open(filename) as f:
-            input= json.load(f)
+            input_data= json.load(f)
         f.close()
     except FileNotFoundError:
             print('No such file')
             exit(1)
-    return input
+    return input_data
 
 def main():
     # defaults
@@ -23,9 +23,9 @@ def main():
         print('Error!')
         print('Usage: python thumbs.py distortedjsonfilename')
         exit()
-    input=getfile(sys.argv[1])
+    input_data=getfile(sys.argv[1])
     url = INI_URL
-    key = next(iter(input))
+    key = next(iter(input_data))
     count = 0
     while True:
         page=requests.get(url, auth=HTTPBasicAuth(USERNAME_AUTH, PASSWORD_AUTH))
@@ -33,14 +33,14 @@ def main():
             raise Exception('Problem fetching page')
         page=page.content
         xmltree = html.fromstring(page)
-        xpath_find,next_page_to_find=input[key]['xpath_test_query'],input[key]['xpath_button_to_click']
+        xpath_find,next_page_to_find=input_data[key]['xpath_test_query'],input_data[key]['xpath_button_to_click']
         #check validity
-        if (xmltree.xpath(xpath_find) == input[key]['xpath_test_result']):
+        if (xmltree.xpath(xpath_find) == input_data[key]['xpath_test_result']):
             print('Move to page',  (count + 1))
             #update URLs
             npageurl = xmltree.xpath(next_page_to_find + '/@href')[0]
             url = INI_URL + npageurl
-            key = input[key]['next_page_expected']
+            key = input_data[key]['next_page_expected']
             count += 1
         else:
             print('ALERT - Can’t move to page ',
